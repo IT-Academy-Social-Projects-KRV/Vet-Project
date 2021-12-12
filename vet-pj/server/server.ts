3ca3bffe34e7c2a3e728202052748fbc3eebccc1
Samwa-ua <askokovskiy@gmail.com>
2021-12-12 11:48:16 +0200

    added server

---
+++ vet-pj/server/server.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    console.log(`${req.ip} ${req.method} ${req.url}`);
+    next();
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send([{ message: 'hello world' }]);
+});
+
+app.listen(4201, '127.0.0.1', function () {
+  console.log('Server now listen on 4201');
+});
