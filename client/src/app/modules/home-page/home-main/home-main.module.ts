50299b02e485a30cd4da987df7bec980805879a0
Andrii <dzura1996das@gmail.com>
2021-12-16 17:44:15 +0200

    created basic markup

---
+++ client/src/app/modules/home-page/home-main/home-main.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeMainRoutingModule } from './home-main-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HomeMainRoutingModule
+  ]
+})
+export class HomeMainModule { }
---
+++ client/src/app/modules/home-page/home-statistic/home-statistic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-statistic',
+  templateUrl: './home-statistic.component.html',
+  styleUrls: ['./home-statistic.component.scss']
+})
+export class HomeStatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
