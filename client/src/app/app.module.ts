3ef369e2afa999f2d0fbaa323d019c4b2b76d283
Andrii <dzura1996das@gmail.com>
2021-12-16 22:05:27 +0200

    fixed header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './modules/header/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeMainComponent } from './modules/home-page/home-main/home-main.component';
+import { HomeStatisticComponent } from './modules/home-page/home-statistic/home-statistic.component';
+import { PetSliderComponent } from './modules/home-page/pet-slider/pet-slider.component';
+import { WrapperComponent } from './modules/home-page/wrapper/wrapper.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCardModule} from '@angular/material/card';
+import { PetWantedSliderComponent } from './modules/home-page/pet-wanted-slider/pet-wanted-slider.component';
+import { HomeDonateComponent } from './modules/home-page/home-donate/home-donate.component';
+import { HomeAboutComponent } from './modules/home-page/home-about/home-about.component';
+import { HomeNewsComponent } from './modules/home-page/home-news/home-news.component';
+import { FooterComponent } from './modules/footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeMainComponent,
+    HomeStatisticComponent,
+    PetSliderComponent,
+    WrapperComponent,
+    PetWantedSliderComponent,
+    HomeDonateComponent,
+    HomeAboutComponent,
+    HomeNewsComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    NgbModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
