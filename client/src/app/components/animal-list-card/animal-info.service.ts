d4e39f475a24864efae4a0fc255514baac3c4ec2
Samwa-ua <askokovskiy@gmail.com>
2021-12-22 18:11:25 +0200

    <chore> replaced some file naming

--- client/src/app/components/animal-list-card/animal-info.service.ts
+++ client/src/app/components/animal-list-card/animal-info.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IAnimalsInfo } from 'src/app/interfaces/animals';
 import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AnimalInfoService {
-  constructor() {}
+  private animalsUrl = 'http://localhost:3200/api/services';
+  constructor(private http: HttpClient) {}
+
+  getAnimalsInfo() {
+    return this.http.get<IAnimalsInfo>(this.animalsUrl).pipe(
+      map((vetDetails) => {
+        return vetDetails;
+      })
+    );
+  }
 }
