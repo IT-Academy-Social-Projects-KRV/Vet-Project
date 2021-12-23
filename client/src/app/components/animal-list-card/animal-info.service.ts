8c81fc131d045dba4d1435a566c7e9033d379b87
Samwa-ua <askokovskiy@gmail.com>
2021-12-23 16:03:19 +0200

    <chore> fixed code

--- client/src/app/components/animal-list-card/animal-info.service.ts
+++ client/src/app/components/animal-list-card/animal-info.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IAnimalsInfo } from 'src/app/interfaces/animals';
 import { ApiPaths, environment } from 'src/environments/environment';
-import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,12 +11,6 @@ export class AnimalInfoService {
   constructor(private http: HttpClient) {}
 
   getAnimalsInfo() {
-    return this.http
-      .get<IAnimalsInfo>(`${this.baseUrl}/${ApiPaths.animals}`)
-      .pipe(
-        map((vetDetails) => {
-          return vetDetails;
-        })
-      );
+    return this.http.get<IAnimalsInfo>(`${this.baseUrl}/${ApiPaths.animals}`);
   }
 }
