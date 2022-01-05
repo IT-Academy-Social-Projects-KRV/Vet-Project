2187e348fb8e861d2cc91e91c1bcba8b5d4aa650
sanderpa <parfeniuksasha09@gmail.com>
2022-01-05 21:17:01 +0200

    Added new component gethome. Added x-icon to site

---
+++ client/src/app/pets/components/pets-gethome/pets-gethome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pets-gethome',
+  templateUrl: './pets-gethome.component.html',
+  styleUrls: ['./pets-gethome.component.scss']
+})
+export class PetsGethomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
