84b4c462912cf320d07de26bab470976faa165c1
Samwa-ua <askokovskiy@gmail.com>
2022-01-05 10:58:47 +0200

    <feat> added several components, styling, images

--- client/src/app/vets/vets.module.ts
+++ client/src/app/vets/vets.module.ts
@@ -9,12 +9,18 @@ import { VetListPageComponent } from './pages/vet-list-page/vet-list-page.compon
 import { VetListCardComponent } from '../shared/components/vet-list-card/vet-list-card.component';
 import { VetInfoService } from '../shared/services/vet-info.service';
 import { VetUnitPageComponent } from './pages/vet-unit-page/vet-unit-page.component';
+import { VeterinariansComponent } from './components/veterinarians/veterinarians.component';
+import { VetServicesComponent } from './components/vet-services/vet-services.component';
+import { VetAnimalsTypeComponent } from './components/vet-animals-type/vet-animals-type.component';
 
 @NgModule({
   declarations: [
     VetListPageComponent,
     VetListCardComponent,
     VetUnitPageComponent,
+    VeterinariansComponent,
+    VetServicesComponent,
+    VetAnimalsTypeComponent,
   ],
   imports: [
     CommonModule,
