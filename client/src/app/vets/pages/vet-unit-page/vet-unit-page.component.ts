569bfcb0375ac9ec84c9df715b52ca0d50100acd
Andrii <dzura1996das@gmail.com>
2022-01-10 14:35:53 +0200

    deleted wrong files

--- client/src/app/vets/pages/vet-unit-page/vet-unit-page.component.ts
+++ client/src/app/vets/pages/vet-unit-page/vet-unit-page.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-import { VetUnitInfoService } from '@shared/services/vet-unit-info.service'
 @Component({
 	selector: 'app-vet-unit-page',
 	templateUrl: './vet-unit-page.component.html',
 	styleUrls: ['./vet-unit-page.component.scss']
 })
 export class VetUnitPageComponent implements OnInit {
-	vetsInfo: any = []
-	id: string
-	info: any = {}
-
-	constructor(private vetInfo: VetUnitInfoService, private _Activatedroute: ActivatedRoute) {
-		this._Activatedroute.paramMap.subscribe(params => {
-			this.id = params.get('id')
-		})
-		this.vetInfo.getVetsUnitInfo(this.id).subscribe(item => {
-			this.vetsInfo = item
-			this.info = this.vetsInfo[0]
-			console.log(this.info)
-		})
-	}
+	constructor() {}
 
 	ngOnInit(): void {}
 }
