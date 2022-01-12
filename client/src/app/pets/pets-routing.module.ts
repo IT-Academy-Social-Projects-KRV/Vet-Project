630230cfc91ee123f9b81d7618d85c2696ce9bf8
sanderpa <parfeniuksasha09@gmail.com>
2022-01-12 16:54:41 +0200

    Merged with develop

--- client/src/app/pets/pets-routing.module.ts
+++ client/src/app/pets/pets-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core'
-<<<<<<< HEAD
 import { RouterModule, Routes } from '@angular/router'
-=======
-import { RouterModule, Routes} from '@angular/router'
->>>>>>> develop
 
 import { AnimalPageComponent } from './pages/animal-list-page/animal-page.component'
 import { AnimalUnitPageComponent } from './pages/animal-unit-page/animal-unit-page.component'
