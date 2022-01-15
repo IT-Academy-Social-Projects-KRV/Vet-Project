4c65c30e401fc6df5bff2067b85ba30e15f30cdc
Samwa-ua <askokovskiy@gmail.com>
2022-01-15 14:38:34 +0200

    <feat> add components(dashboard, page for adding vets)

---
+++ client/src/app/admin-page/components/admin-edit-vet/admin-edit-vet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit-vet',
+  templateUrl: './admin-edit-vet.component.html',
+  styleUrls: ['./admin-edit-vet.component.scss']
+})
+export class AdminEditVetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/admin-page/components/admin-nav/admin-nav.component.ts
+++ client/src/app/admin-page/components/admin-nav/admin-nav.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
 
 @Component({
-  selector: 'app-admin-nav',
-  templateUrl: './admin-nav.component.html',
-  styleUrls: ['./admin-nav.component.scss']
+	selector: 'app-admin-nav',
+	templateUrl: './admin-nav.component.html',
+	styleUrls: ['./admin-nav.component.scss']
 })
 export class AdminNavComponent {
+	menuItems = ['dashboard', 'sales', 'orders', 'customers', 'products']
+	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+		map(result => result.matches),
+		shareReplay()
+	)
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+	constructor(private breakpointObserver: BreakpointObserver) {}
 }
