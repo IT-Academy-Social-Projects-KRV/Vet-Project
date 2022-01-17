e381adffa707ddc6e20f69f666260e17526d2775
Andriy <Shushniak96@gmail.com>
2022-01-17 15:14:42 +0200

    <feat> added error handler to email input

--- client/src/app/admin-page/components/login-modal/login-modal.component.ts
+++ client/src/app/admin-page/components/login-modal/login-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
 
 @Component({
 	selector: 'app-login-modal',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core'
 	styleUrls: ['./login-modal.component.scss']
 })
 export class LoginModalComponent {
+	email = new FormControl('', [Validators.required, Validators.email])
 	hide: boolean = true
+	getErrorMessage() {
+		if (this.email.hasError('required')) {
+			return 'You must enter a value'
+		}
+
+		return this.email.hasError('email') ? 'Not a valid email' : ''
+	}
 }
