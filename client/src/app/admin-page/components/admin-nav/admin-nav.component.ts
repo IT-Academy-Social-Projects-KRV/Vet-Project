d99fa34379e6dab8f1e7af0561b4f0a2a39467f9
sanderpa <parfeniuksasha09@gmail.com>
2022-01-18 14:06:39 +0200

    fix rules on eslintrc.json

--- client/src/app/admin-page/components/admin-nav/admin-nav.component.ts
+++ client/src/app/admin-page/components/admin-nav/admin-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core'
+import { Component } from '@angular/core'
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
@@ -11,7 +11,7 @@ import { map, shareReplay } from 'rxjs/operators'
 export class AdminNavComponent {
 	loadedFeature = ''
 	onSelect(feature: string) {
-		this.loadedFeature = feature;
+		this.loadedFeature = feature
 	}
 	// menuItems = ['dashboard', 'sales', 'orders', 'customers', 'products']
 	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
--- client/src/app/admin-page/pages/admin-unit/admin-unit.component.spec.ts
+++ client/src/app/admin-page/pages/admin-unit/admin-unit.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* eslint-disable no-undef */
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AdminUnitComponent } from './admin-unit.component';
+import { AdminUnitComponent } from './admin-unit.component'
 
 describe('AdminUnitComponent', () => {
-  let component: AdminUnitComponent;
-  let fixture: ComponentFixture<AdminUnitComponent>;
+	let component: AdminUnitComponent
+	let fixture: ComponentFixture<AdminUnitComponent>
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminUnitComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [AdminUnitComponent]
+		}).compileComponents()
+	})
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminUnitComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AdminUnitComponent)
+		component = fixture.componentInstance
+		fixture.detectChanges()
+	})
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+	it('should create', () => {
+		expect(component).toBeTruthy()
+	})
+})
