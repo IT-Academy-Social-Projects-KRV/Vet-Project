483dd8fbd8e65b9bf9339c556c3529ebc40522c3
sanderpa <parfeniuksasha09@gmail.com>
2022-01-19 15:29:49 +0200

    Merge branch 'develop' into feat/sprint_2_13_1_add_animal_card_carousel_component

--- client/src/app/admin-page/pages/admin-unit/admin-unit.component.ts
+++ client/src/app/admin-page/pages/admin-unit/admin-unit.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
 	selector: 'app-admin-unit',
 	templateUrl: './admin-unit.component.html',
 	styleUrls: ['./admin-unit.component.scss']
 })
-export class AdminUnitComponent implements OnInit {
+export class AdminUnitComponent {
 	constructor() {}
-
-	ngOnInit(): void {}
 }
