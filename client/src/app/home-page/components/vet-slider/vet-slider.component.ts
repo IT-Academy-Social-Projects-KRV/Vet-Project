8d367d8865ad8655684eb2fd46627cedc97a1afb
sanderpa <parfeniuksasha09@gmail.com>
2022-01-19 20:20:42 +0200

    fix problem with service in vet-slider component

--- client/src/app/home-page/components/vet-slider/vet-slider.component.ts
+++ client/src/app/home-page/components/vet-slider/vet-slider.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { FormBuilder, FormGroup } from '@angular/forms'
-import { VetInfoService } from '@shared/services/vet-info.service'
+import { ApiServices } from '@shared/services/api.service'
 
 @Component({
 	selector: 'app-vet-slider',
 	templateUrl: './vet-slider.component.html',
 	styleUrls: ['./vet-slider.component.scss']
 })
-export class VetSliderComponent implements OnInit {
+export class VetSliderComponent {
 	vetsInfo: any = []
-	constructor(private vetInfo: VetInfoService) {
-		this.vetInfo.getVetDetails().subscribe(item => {
+	constructor(private apiServices: ApiServices) {
+		this.apiServices.getVetDetails().subscribe(item => {
 			this.vetsInfo = item
 		})
 	}
-
-	ngOnInit(): void {}
 }
