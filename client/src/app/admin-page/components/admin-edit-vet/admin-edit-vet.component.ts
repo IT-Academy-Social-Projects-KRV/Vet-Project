f03016ccedd321b9e2f12031503ada88517ad8d7
sanderpa <parfeniuksasha09@gmail.com>
2022-01-21 19:14:46 +0200

    Binded input field to method. Added validation for email

--- client/src/app/admin-page/components/admin-edit-vet/admin-edit-vet.component.ts
+++ client/src/app/admin-page/components/admin-edit-vet/admin-edit-vet.component.ts
@@ -3,55 +3,23 @@ import { FormControl, Validators } from '@angular/forms'
 import { ApiServices } from '@shared/services/api.service'
 import { IVetsUnitInfo } from '@shared/interfaces/vets-unit'
 
-interface Animal {
-	name: string
-}
-interface Specialization {
-	name: string
-}
-
 @Component({
 	selector: 'app-admin-edit-vet',
 	templateUrl: './admin-edit-vet.component.html',
 	styleUrls: ['./admin-edit-vet.component.scss']
 })
 export class AdminEditVetComponent {
-	typesOfShoes: string[] = [
-		'Хірургія',
-		'Травматологія',
-		'Вакцинування',
-		'Офтмальмолог',
-		'Стерилізація'
-	]
-
-	animalControl = new FormControl('', Validators.required)
 	selectFormControl = new FormControl('', Validators.required)
-	animals: Animal[] = [
-		{ name: 'Коти' },
-		{ name: 'Собаки' },
-		{ name: 'Гризуни' },
-		{ name: 'Рептилії' },
-		{ name: 'Птахи' }
-	]
-	vetSpecialization = new FormControl('', Validators.required)
-	selectSpecializationControl = new FormControl('', Validators.required)
-	specialization: Specialization[] = [
-		{ name: 'Хірургія' },
-		{ name: 'Травматологія' },
-		{ name: 'Вакцинування' },
-		{ name: 'Офтмальмолог' },
-		{ name: 'Стерилізація' }
-	]
 
 	constructor(private apiServices: ApiServices) {}
 	item: IVetsUnitInfo
-	onCreateClinic(): void {
+	onCreateClinic(form): void {
 		this.item = {
-			title: 'string',
-			adress: 'string',
-			phone: 'string',
-			email: 'string',
-			map_link: 'string'
+			title: form.title,
+			adress: form.adress,
+			phone: form.phone,
+			email: form.email,
+			map_link: form.map_link
 		}
 		// Send Http request
 		this.apiServices.postNewClinic(this.item)
