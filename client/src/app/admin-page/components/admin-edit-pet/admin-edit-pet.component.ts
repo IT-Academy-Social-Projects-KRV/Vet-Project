eaba91f3f5af157f59ef62ec58e11acdf1c41f79
Andriy <Shushniak96@gmail.com>
2022-01-22 11:41:40 +0200

    <feat> connected input values with post method

--- client/src/app/admin-page/components/admin-edit-pet/admin-edit-pet.component.ts
+++ client/src/app/admin-page/components/admin-edit-pet/admin-edit-pet.component.ts
@@ -1,27 +1,7 @@
 import { Component, ViewChild } from '@angular/core'
 import { NgForm } from '@angular/forms'
-import { MatTable } from '@angular/material/table'
 import { IAnimalsUnitInfo } from '@shared/interfaces/animals-unit'
 import { ApiServices } from '@shared/services/api.service'
-
-export interface PeriodicElement {
-	name: string
-	position: number
-	weight: number
-	symbol: string
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-	{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-	{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-	{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-	{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-	{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
-]
 
 @Component({
 	selector: 'app-admin-edit-pet',
@@ -29,16 +9,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 	styleUrls: ['./admin-edit-pet.component.scss']
 })
 export class AdminEditPetComponent {
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol']
-	dataSource = [...ELEMENT_DATA]
-
-	@ViewChild(MatTable) table: MatTable<PeriodicElement>
-
 	constructor(private apiServices: ApiServices) {}
 	item: IAnimalsUnitInfo
-	onSubmit(form): void {
+	onSubmit(form: IAnimalsUnitInfo): void {
 		this.item = {
-			name: form.title,
+			name: form.name,
 			shelterName: 'string',
 			curator: 'string',
 			gender: form.gender,
@@ -51,15 +26,4 @@ export class AdminEditPetComponent {
 		this.apiServices.postNewAnimal(this.item)
 	}
 	onCreateAnimal(): void {}
-
-	addData() {
-		const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length)
-		this.dataSource.push(ELEMENT_DATA[randomElementIndex])
-		this.table.renderRows()
-	}
-
-	removeData() {
-		this.dataSource.pop()
-		this.table.renderRows()
-	}
 }
