f90c28629a9d8cb77931324e879cda048a087e46
Samwa-ua <askokovskiy@gmail.com>
2022-01-23 18:00:01 +0200

    <feat> add tabs and filter markup to edit-pets page

--- client/src/app/admin-page/components/admin-edit-pet/admin-edit-pet.component.ts
+++ client/src/app/admin-page/components/admin-edit-pet/admin-edit-pet.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, ViewChild } from '@angular/core'
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { IAnimalsUnitInfo } from '@shared/interfaces/animals-unit'
 import { ApiServices } from '@shared/services/api.service'
 import { Observable } from 'rxjs'
-import { NgForm } from '@angular/forms'
 import { IAnimalsInfo } from '@shared/interfaces/animals'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
 	selector: 'app-admin-edit-pet',
@@ -12,16 +14,32 @@ import { IAnimalsInfo } from '@shared/interfaces/animals'
 	styleUrls: ['./admin-edit-pet.component.scss']
 })
 export class AdminEditPetComponent implements OnInit {
-	displayedColumns: string[] = ['id', 'name', 'weight', 'breed', 'age', 'curator']
-
+	displayedColumns: string[] = ['id', 'name', 'gender', 'breed', 'age', 'curator']
 	animalsInfo$: Observable<IAnimalsInfo[]>
-
 	petsInfo$: Observable<IAnimalsUnitInfo>
+
+	dataSource: MatTableDataSource<IAnimalsInfo>
+
+	@ViewChild(MatPaginator) paginator: MatPaginator
+	@ViewChild(MatSort) sort: MatSort
 
 	constructor(private apiServices: ApiServices) {}
 
 	ngOnInit(): void {
 		this.animalsInfo$ = this.apiServices.getAnimalsInfo()
+	}
+
+	ngAfterViewInit() {
+		this.dataSource.paginator = this.paginator
+		this.dataSource.sort = this.sort
+	}
+	applyFilter(event: Event) {
+		const filterValue = (event.target as HTMLInputElement).value
+		this.dataSource.filter = filterValue.trim().toLowerCase()
+
+		if (this.dataSource.paginator) {
+			this.dataSource.paginator.firstPage()
+		}
 	}
 	onEdit() {
 		// this.Activatedroute.paramMap.subscribe(params => {
@@ -29,16 +47,6 @@ export class AdminEditPetComponent implements OnInit {
 		// })
 	}
 
-	addData() {
-		// const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length)
-		// this.dataSource.push(ELEMENT_DATA[randomElementIndex])
-		// this.table.renderRows()
-	}
-
-	removeData() {
-		// this.dataSource.pop()
-		// this.table.renderRows()
-	}
 	item: IAnimalsUnitInfo
 	curators: any[] = ['Куратор 1', 'Куратор 2']
 	onSubmit(form: IAnimalsUnitInfo): void {
