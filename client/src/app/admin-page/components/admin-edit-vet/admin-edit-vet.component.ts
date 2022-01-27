25cf835f230776eaa795e17df2a5359eef9fc06e
sanderpa <parfeniuksasha09@gmail.com>
2022-01-27 11:10:42 +0200

    Change language in several component

--- client/src/app/admin-page/components/admin-edit-vet/admin-edit-vet.component.ts
+++ client/src/app/admin-page/components/admin-edit-vet/admin-edit-vet.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { FormControl, Validators } from '@angular/forms'
 import { ApiServices } from '@shared/services/api.service'
 import { IVetsUnitInfo } from '@shared/interfaces/vets-unit'
+import { IVetsInfo } from '@shared/interfaces/vetInfo'
+import { Observable } from 'rxjs'
+
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { MatTableDataSource } from '@angular/material/table'
 
 @Component({
 	selector: 'app-admin-edit-vet',
 	templateUrl: './admin-edit-vet.component.html',
 	styleUrls: ['./admin-edit-vet.component.scss']
 })
-export class AdminEditVetComponent {
+export class AdminEditVetComponent implements OnInit {
 	selectFormControl = new FormControl('', Validators.required)
 
+	displayedColumns: string[] = ['id', 'title', 'adress', 'phone', 'email']
+	IVetsInfo$: Observable<Array<IVetsInfo>>
+
+	dataSource = new MatTableDataSource<IVetsInfo[]>()
+	@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
+	@ViewChild(MatSort, { static: true }) sort: MatSort
+
 	constructor(private apiServices: ApiServices) {}
+
+	ngOnInit(): void {
+		this.IVetsInfo$ = this.apiServices.getVetDetails()
+	}
+
+	applyFilter(event: Event) {
+		const filterValue = (event.target as HTMLInputElement).value
+		this.dataSource.filter = filterValue.trim().toLowerCase()
+
+		if (this.dataSource.paginator) {
+			this.dataSource.paginator.firstPage()
+		}
+	}
+
 	item: IVetsUnitInfo
 
 	onCreateClinic(form): void {
@@ -24,5 +51,9 @@ export class AdminEditVetComponent {
 		}
 		// Send Http request
 		this.apiServices.postNewClinic(this.item)
+	}
+
+	onDeleteClinic(): void {
+		//...
 	}
 }
