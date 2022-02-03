098c9c54539acab2207e0ff2ca4939736b060162
Samwa-ua <askokovskiy@gmail.com>
2022-02-03 14:14:07 +0200

    <fix> removed width from carousel container

--- client/src/app/admin-page/components/admin-dashboard/admin-dashboard.component.ts
+++ client/src/app/admin-page/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { map } from 'rxjs/operators'
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout'
+import { ApiServices } from '@shared/services/api.service'
+import { Observable, Subscription } from 'rxjs'
 
 @Component({
 	selector: 'app-admin-dashboard',
@@ -9,25 +11,57 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout'
 })
 export class AdminDashboardComponent {
 	/** Based on the screen size, switch from standard to one column per row */
+	vetsAmount: any
+	// cards: any
+
 	cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
 		map(({ matches }) => {
 			if (matches) {
 				return [
-					{ title: 'К-сть тварин', cols: 1, rows: 1 },
-					{ title: 'К-сть клінік', cols: 1, rows: 1 },
-					{ title: 'К-сть волонтерів', cols: 1, rows: 1 },
-					{ title: 'Card 4', cols: 1, rows: 1 }
+					{ title: 'Тварин:', cols: 1, rows: 1, amount: this.vetsAmount },
+					{ title: 'Клінік:', cols: 1, rows: 1, amount: 2 },
+					{ title: 'Волонтерів:', cols: 1, rows: 1, amount: 3 }
 				]
 			}
 
 			return [
-				{ title: 'К-сть тварин', cols: 1, rows: 1 },
-				{ title: 'К-сть клінік', cols: 1, rows: 1 },
-				{ title: 'К-сть волонтерів', cols: 1, rows: 1 },
-				{ title: 'Card 4', cols: 1, rows: 1 }
+				{ title: 'Тварин:', cols: 1, rows: 1, amount: this.vetsAmount },
+				{ title: 'Клінік:', cols: 1, rows: 1, amount: 2 },
+				{ title: 'Волонтерів:', cols: 1, rows: 1, amount: 3 }
 			]
 		})
 	)
 
-	constructor(private breakpointObserver: BreakpointObserver) {}
+	// eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+	ngOnInit(): void {
+		this.vetsAmount = this.apiservice.getAnimalsInfo().subscribe(result => {
+			this.vetsAmount = result.length
+			console.log(this.vetsAmount)
+		})
+	}
+
+	// ngOnInit(): void {
+	// 	this.apiservice.getAnimalsInfo().subscribe(result => {
+	// 		this.cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+	// 			map(({ matches }) => {
+	// 				if (matches) {
+	// 					return [
+	// 						{ title: 'Тварин:', cols: 1, rows: 1, amount: this.vetsAmount },
+	// 						{ title: 'Клінік:', cols: 1, rows: 1, amount: 2 },
+	// 						{ title: 'Волонтерів:', cols: 1, rows: 1, amount: 3 }
+	// 					]
+	// 				}
+
+	// 				return [
+	// 					{ title: 'Тварин:', cols: 1, rows: 1, amount: this.vetsAmount },
+	// 					{ title: 'Клінік:', cols: 1, rows: 1, amount: 2 },
+	// 					{ title: 'Волонтерів:', cols: 1, rows: 1, amount: 3 }
+	// 				]
+	// 			})
+	// 		)
+	// 		this.vetsAmount = result.length
+	// 		console.log(this.vetsAmount)
+	// 	})
+	// }
+	constructor(private breakpointObserver: BreakpointObserver, private apiservice: ApiServices) {}
 }
