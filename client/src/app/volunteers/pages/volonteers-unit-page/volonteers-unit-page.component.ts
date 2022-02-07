ece1e208919bd90acb165ebc98cd67964ce24290
Max Kobzar <nimbus2219@gmail.com>
2022-02-07 10:11:00 +0200

    create unit page for volonteers

--- client/src/app/volunteers/pages/volonteers-unit-page/volonteers-unit-page.component.ts
+++ client/src/app/volunteers/pages/volonteers-unit-page/volonteers-unit-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ApiServices } from '@shared/services/api.service'
+
+import { ActivatedRoute } from '@angular/router'
+import { IVolonteersInfo } from '@shared/interfaces/volonteers'
 
 @Component({
-  selector: 'app-volonteers-unit-page',
-  templateUrl: './volonteers-unit-page.component.html',
-  styleUrls: ['./volonteers-unit-page.component.scss']
+	selector: 'app-volonteers-unit-page',
+	templateUrl: './volonteers-unit-page.component.html',
+	styleUrls: ['./volonteers-unit-page.component.scss']
 })
 export class VolonteersUnitPageComponent implements OnInit {
+	id: string
 
-  constructor() { }
+	volonteersInfo$: Observable<IVolonteersInfo>
 
-  ngOnInit(): void {
-  }
-
+	constructor(private apiServices: ApiServices, private Activatedroute: ActivatedRoute) {
+		this.Activatedroute.paramMap.subscribe(params => {
+			this.id = params.get('id')
+		})
+	}
+	ngOnInit(): void {
+		this.volonteersInfo$ = this.apiServices.getVolonteersUnitInfo(this.id)
+	}
 }
