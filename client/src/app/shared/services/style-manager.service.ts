edcd90a82bb7e31754a7426c70aaf428b3727cad
Samwa-ua <askokovskiy@gmail.com>
2022-02-14 12:56:19 +0200

    <chore> cleared extencive code

--- client/src/app/shared/services/style-manager.service.ts
+++ client/src/app/shared/services/style-manager.service.ts
@@ -3,51 +3,24 @@ import { Injectable } from '@angular/core'
 @Injectable({ providedIn: 'root' })
 export class StyleManager {
 	public iconChange: boolean
-	isDark
+	public isDark: boolean
 	detectColorScheme() {
-		let dark = '(prefers-color-scheme: dark)'
-		let defaultIcon = window.matchMedia(`${dark}`).matches
-		if (defaultIcon) this.iconChange = defaultIcon
+		const defaultIcon = window.matchMedia('(prefers-color-scheme: dark)').matches
+		if (defaultIcon) {
+			this.iconChange = defaultIcon
+		}
 	}
 
 	constructor() {
 		this.detectColorScheme()
 	}
-	toggleDarkTheme() {
+	toggleDarkTheme(): void {
 		if (this.isDark) {
-			this.removeStyle('light-theme')
 			document.body.classList.remove('light-theme')
 			this.isDark = false
 		} else {
-			const href = 'light-theme.css'
-			getLinkElementForKey('light-theme').setAttribute('href', href)
 			document.body.classList.add('light-theme')
 			this.isDark = true
 		}
 	}
-	removeStyle(key: string) {
-		const existingLinkElement = getExistingLinkElementByKey(key)
-		if (existingLinkElement) {
-			document.head.removeChild(existingLinkElement)
-		}
-	}
-}
-function getLinkElementForKey(key: string) {
-	return getExistingLinkElementByKey(key) || createLinkElementWithKey(key)
-}
-
-function getExistingLinkElementByKey(key: string) {
-	return document.head.querySelector(`link[rel="stylesheet"].${getClassNameForKey(key)}`)
-}
-
-function createLinkElementWithKey(key: string) {
-	const linkEl = document.createElement('link')
-	linkEl.setAttribute('rel', 'stylesheet')
-	linkEl.classList.add(getClassNameForKey(key))
-	document.head.appendChild(linkEl)
-	return linkEl
-}
-
-function getClassNameForKey(key: string) {
-	return `style-manager-${key}`
 }
